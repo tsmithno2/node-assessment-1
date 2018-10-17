fbf90c671e70c9732b976ad5f3a4cebaeac6cbbf
Trevor Smith <t.smithno2@gmail.com>
2018-10-17 11:29:43 -0600

    Boilerplate for controller down

---
+++ usersCtrl.js
@@ -0,0 +1,5 @@
+//require the userData array from the userData.json file
+const userData = require("./userData.json");
+
+//when the endpoints are hit we will use these functions to perform the desired actions
+module.exports = {}

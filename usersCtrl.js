81b67a7eb319109739df7edb7addde3576c5a7d1
Trevor Smith <t.smithno2@gmail.com>
2018-10-17 13:45:53 -0600

    Still working on the first one, hourly commit

--- usersCtrl.js
+++ usersCtrl.js
@@ -7,45 +7,66 @@ module.exports = {
 
     //Going to put simple console.logs in each function to make sure we are at least hitting the endpoints. 
     //If all work, then we will move to writing the actual code needed to pass the postman endpoints
-    //Success is defined by the correct console.log apearing in the console
-    getAllUsers: (req, res) => { 
-        console.log("We got here to function getAllUsers")
+   
+    getAllUsers: (req, res) => {
+        console.log("We got here to function getAllUsers");
+        //We need to be ready to see if a query is used. 
+        //The possibilities are,
+        //age. If used return all user objects with "age" property less than specified
+        //lastname. If used return all user objects whose "last_name" property match
+        //email. If used return all user objects whose "email" proptery match
+        //favorites. If used return all user objects that contain the query in their favorites array
+        //If no query, return the entire array
+
+        //Set the responce variable to be an empty array and push all mnatches in. 
+        //If no query send the whole userData array in the final else statement.
+        let responce = [];
+
+        //Check conditions first, and if none are met send the userData array
+        //REMINDER ALL QUERIES WILL BE STRINGS IN THE JSON DOCU8MENT THERE ARE INTEGERS WE MAY NEED TO CHANGE THE STRINGS TO INTEGERS
+        if (req.query.age){
+            let ageCheck = parseInt(req.query.age)
+            responce = userData.filter(user => user.age < ageCheck);
+            console.log("Should be done with age", responce);
+            res.status(200).send(responce);
+        } else if (req.query.lastname){
+            responce = userData.filter(user => user.last_name === req.query.lastname)
+            console.log("Should be done with last_name ", responce)
+        } else if (req.query.email){
+            responce = userData.filter(user => user.email === req.query.email)
+        } else if (req.query.favorites){
+            responce = userData.filter(user => user.favorites.includes(req.query.favorites))
+        } else {
+            res.status.status(200).send(userData)
+        }
     }, 
-    //success
 
     getSpecUser: (req, res) => { 
         console.log("We got here to function getSpecUser")
     },
-    //success
 
     getAdmins: (req, res) => {
         console.log("We got here to function getAdmins")
     },
-    //success
 
     getNonAdmins: (req, res) => {
         console.log("We got here to function getNonAdmins")
     },
-    //success
 
     getCertainType: (req, res) => {
         console.log("We got here to function getCertainType")
     },
-    //success
 
     updateUser: (req, res) => {
         console.log("We got here to function updateUser")
     },
-    //success
 
     createNewUser: (req, res) => {
         console.log("We got here to function createNewUser")
     },
-    //success
 
     deleteUser: (req, res) => {
         console.log("We got here to function deleteUser")
     },
-    //success
  
 }

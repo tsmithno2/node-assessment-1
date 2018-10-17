d6369b7b6e85da195cc9e1bd5270f76830d46f2b
Trevor Smith <t.smithno2@gmail.com>
2018-10-17 12:14:07 -0600

    pre-test of endpoint functions on the usersCtrl.js file

--- usersCtrl.js
+++ usersCtrl.js
@@ -1,5 +1,41 @@
-//require the userData array from the userData.json file
+//Require the userData array from the userData.json file
 const userData = require("./userData.json");
 
-//when the endpoints are hit we will use these functions to perform the desired actions
-module.exports = {}
+//When the endpoints are hit we will use these functions to perform the desired actions
+//The functions are all named in server.js. Copy and paste those names so there is no naming confusion here
+module.exports = {
+    //Going to put simple console.logs in each function to make sure we are at least hitting the endpoints. 
+    //If all work, then we will move to writing the actual code needed to pass the postman endpoints
+    getAllUsers: (req, res) => { 
+        console.log("We got here to function getAllUsers")
+    }, 
+
+    getSpecUser: (req, res) => { 
+        console.log("We got here to function getSpecUser")
+    },
+
+    getAdmins: (req, res) => {
+        console.log("We got here to function getAdmins")
+    },
+
+    getNonAdmins: (req, res) => {
+        console.log("We got here to function getNonAdmins")
+    },
+
+    getCertainType: (req, res) => {
+        console.log("We got here to function getCertainType")
+    },
+
+    updateUser: (req, res) => {
+        console.log("We got here to function updateUser")
+    },
+
+    createNewUser: (req, res) => {
+        console.log("We got here to function createNewUser")
+    },
+
+    deleteUser: (req, res) => {
+        console.log("We got here to function deleteUser")
+    },
+ 
+}
